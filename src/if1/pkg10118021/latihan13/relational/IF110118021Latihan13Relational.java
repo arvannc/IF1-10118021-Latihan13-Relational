b0262622bc23577d533f5a1a5202405fd8f134d9
M Arvan Nurcahyana <55648161+arvannc@users.noreply.github.com>
2019-09-24 18:41:30 +0700

    Add files via upload

---
+++ src/if1/pkg10118021/latihan13/relational/IF110118021Latihan13Relational.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118021.latihan13.relational;
+
+/**
+ *
+ * @author Arvan
+ * NAMA     : M Arvan Nurcahyana
+ * KELAS    : IF-1
+ * NIM      : 10118021
+ * Deskripsi Program : Relational
+ */
+public class IF110118021Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
